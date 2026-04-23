c4975df2ca5ffd0368d4a59d3d8fce3ecda7e584
irwandikamalik <irwandikamalikfadduani@gmail.com>
2026-04-23 17:40:35 +0700

    feat: add certificate section

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Typing effect
 const text = ["Robotics Engineer", "IoT Developer", "ROS Developer"];
-let i = 0, j = 0, current = "", isDeleting = false;
+let i = 0,
+  j = 0,
+  current = "",
+  isDeleting = false;
+
+document.addEventListener("DOMContentLoaded", function () {
+  type();
+});
 
 function type() {
   current = text[i];
@@ -13,7 +20,7 @@ function type() {
     if (!isDeleting) i = (i + 1) % text.length;
   }
 
-  setTimeout(type, isDeleting ? 50 : 100);
+  setTimeout(type, isDeleting ? 150 : 50);
 }
 
 // Scroll
@@ -23,20 +30,21 @@ function scrollToSection(id) {
 
 // Modal data
 const data = {
-  robot: "Menggunakan encoder + IMU + EKF untuk estimasi posisi robot secara akurat.",
+  robot:
+    "Menggunakan encoder + IMU + EKF untuk estimasi posisi robot secara akurat.",
   udp: "Komunikasi real-time STM32 ke ROS menggunakan UDP + W5500 dengan latency rendah.",
   iot: "Monitoring pH, TDS, suhu dengan MQTT dan dashboard Node-RED.",
-  yolo: "Deteksi objek menggunakan YOLOv8 dan ditampilkan di web monitoring."
+  yolo: "Deteksi objek menggunakan YOLOv8 dan ditampilkan di web monitoring.",
 };
 
 function openModal(key) {
-  document.getElementById('modal').classList.remove('hidden');
-  document.getElementById('modal-title').textContent = key.toUpperCase();
-  document.getElementById('modal-desc').textContent = data[key];
+  document.getElementById("modal").classList.remove("hidden");
+  document.getElementById("modal-title").textContent = key.toUpperCase();
+  document.getElementById("modal-desc").textContent = data[key];
 }
 
 function closeModal() {
-  document.getElementById('modal').classList.add('hidden');
+  document.getElementById("modal").classList.add("hidden");
 }
 
 // Start typing
